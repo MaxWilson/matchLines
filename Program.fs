c15cab4329591e85343df70fbee0fa0fcc49ee73
Max Wilson <maxw@microsoft.com>
2022-09-08 13:55:39 -0700

    Don't crash on errors, just skip and print
    Allow 'without' flag to exclude certain lines, e.g. Async -without await

--- Program.fs
+++ Program.fs
@@ -40,7 +40,7 @@ module Seq =
 [<EntryPoint>]
 let main argv =
     match argv |> List.ofArray with
-    | Optional "d" (dir, Optional "de" (de, Optional "df" (df, Optional "f" (fileFilter, Replace(replacements, (_::_ as patterns)))))) ->
+    | Optional "d" (dir, Optional "de" (de, Optional "df" (df, Optional "f" (fileFilter, Optional "without"(without, Replace(replacements, (_::_ as patterns))))))) ->
         let rootDir =
             match dir with
             | None -> Environment.CurrentDirectory
@@ -52,46 +52,53 @@ let main argv =
             | None -> fun _ -> true
         let directoryFilter =
             match df, de with
-            | Some include, Some exclude -> (fun x -> isMatch include x && not (isMatch exclude x))
+            | Some include', Some exclude -> (fun x -> isMatch include' x && not (isMatch exclude x))
             | None, Some exclude -> isMatch exclude >> not
-            | Some include, None-> isMatch include
+            | Some include', None-> isMatch include'
             | None, None -> fun _ -> true
+        let accept =
+            match without with
+            | None -> fun line -> true // always accept if there's no without clause
+            | Some without -> isMatch without >> not
         let rec recur dir =
             for file in Directory.EnumerateFiles dir |> Seq.filter pathFilter do
                 let fileName = Path.GetFileName file
                 // has to match all patterns in order to qualify for printout
-                if File.ReadLines file |> Seq.filter (fun ln -> patterns |> Seq.every(flip isMatch ln)) |> Seq.isEmpty |> not then
+                if File.ReadLines file |> Seq.filter (fun ln -> accept ln && patterns |> Seq.every(flip isMatch ln)) |> Seq.isEmpty |> not then
                     printfn "%s" (file.Replace(rootDir + Path.DirectorySeparatorChar.ToString(), "")) // trim root directory from output
-                    if replacements |> Seq.isEmpty then
-                        for line in File.ReadLines file |> Seq.filter (fun ln -> patterns |> Seq.every(flip isMatch ln)) do
-                            printfn "%s" line
-                    else
-                        let mutable modified = false
-                        let mutable lines = File.ReadAllLines file
-                        for ix in [0..lines.Length - 1] do
-                            let mutable isMatch = false
-                            let line = lines.[ix]
-                            for rep in replacements do
-                                match rep with
-                                | Some key, value ->
-                                    if Regex.IsMatch(line, key) then
-                                        lines.[ix] <- Regex.Replace(line, key, value)
-                                        modified <- true
-                                        isMatch <- true
-                                | None, value ->
-                                    let mutable line = line
-                                    for pattern in patterns do
-                                        if Regex.IsMatch(line, pattern) then
-                                            lines.[ix] <- Regex.Replace(line, pattern, value)
+                    try
+                        if replacements |> Seq.isEmpty then
+                            for line in File.ReadLines file |> Seq.filter (fun ln -> accept ln && patterns |> Seq.every(flip isMatch ln)) do
+                                printfn "%s" line
+                        else
+                            let mutable modified = false
+                            let mutable lines = File.ReadAllLines file
+                            for ix in [0..lines.Length - 1] do
+                                let mutable isMatch = false
+                                let line = lines.[ix]
+                                for rep in replacements do
+                                    match rep with
+                                    | Some key, value ->
+                                        if Regex.IsMatch(line, key) then
+                                            lines.[ix] <- Regex.Replace(line, key, value)
                                             modified <- true
-                                            isMatch <- true
-                            if isMatch then
-                                printfn "%s" (lines.[ix])
-                        if modified then
-                            File.WriteAllLines(file, lines)
-                    printfn ""
+                                            isMatch <- accept lines[ix]
+                                    | None, value ->
+                                        let mutable line = line
+                                        for pattern in patterns do
+                                            if Regex.IsMatch(line, pattern) then
+                                                lines.[ix] <- Regex.Replace(line, pattern, value)
+                                                modified <- true
+                                                isMatch <- accept lines[ix]
+                                if isMatch then
+                                    printfn "%s" (lines.[ix])
+                            if modified then
+                                File.WriteAllLines(file, lines)
+                        printfn ""
+                    with err ->
+                        printfn $"Skipping due to error: '{err}'"
             for d in Directory.EnumerateDirectories dir |> Seq.filter directoryFilter do
                 recur d
         recur rootDir
-    | _ -> printfn "Usage: matchLines [-d <directory>] [-df <directoryFilter>] [-de <directories to exclude>] [-f <pathFilter>] [-replace [<pattern>] <value>] <patterns...>"
+    | _ -> printfn "Usage: matchLines [-d <directory>] [-df <directoryFilter>] [-de <directories to exclude>] [-f <pathFilter>] [-replace [<pattern>] <value>] <patterns...> [without <pattern to exclude>]"
     0 // return an integer exit code
